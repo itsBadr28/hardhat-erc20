f2028ef048beb9451495e06a955985707bcda0c9
itsBadr <adbadr28@gmail.com>
2022-11-26 21:11:31 +0100

    add transfer functions

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -8,6 +8,7 @@ contract ERC20 {
     // uint8 public decimals;
 
     mapping (address => uint256) public balanceOf;
+    mapping (address => mapping(address => unit256)) public allowance;
 
     constructor(string memory _name, string memory _symbol) {
         name = _name;
@@ -19,11 +20,35 @@ contract ERC20 {
     }
 
     function transfer(address recipient,unit256 amount) external returns (bool) {
+        
+        return _transfer(msg.sender, recipient, amount);
+
+    }
+
+    function transferFrom(address sender, address recipient,unit256 amount) external returns (bool) {
+        
+        uint256 currentAllowance = allowance[sender][msg.sender];
+        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+        allowance[sender][msg.sender] = currentAllowance - amount;
+        return _transfer(msg.sender, recipient, amount);
+
+    }
+    
+    function approve(address spender, uint256 amount) external returns (bool) {
+        
+        require(spender != address(0), "ERC20: approve to the zero address");
+        allowance[msg.sender][spender] = amount;
+        return true;
+    }
+
+    function _transfer(address sender, address recipient,unit256 amount) external returns (bool) {
+        
         require(recipient != address(0), "ERC20: transfer to the zero address");
         uint256 senderBalance = balanceOf[msg.sender];
         require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
-        balanceOf[msg.sender] = senderBalance - amount;
+        balanceOf[sender] = senderBalance - amount;
         balanceOf[recipient] += amount;
         return true;
+
     }
 } 
