8af2a7ea1e024d40eaadd8f5027be29e5c62d5f3
itsBadr <adbadr28@gmail.com>
2022-11-26 20:47:30 +0100

    add mapping and function transfer

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -5,7 +5,9 @@ contract ERC20 {
     uint256 public totalSupply;
     string public name;
     string public symbol;
-    uint8 public decimals;
+    // uint8 public decimals;
+
+    mapping (address => uint256) public balanceOf;
 
     constructor(string memory _name, string memory _symbol) {
         name = _name;
@@ -15,4 +17,13 @@ contract ERC20 {
     function decimals() external pure returns (unit8) {
         return 18;
     }
-}
+
+    function transfer(address recipient,unit256 amount) external returns (bool) {
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        uint256 senderBalance = balanceOf[msg.sender];
+        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+        balanceOf[msg.sender] = senderBalance - amount;
+        balanceOf[recipient] += amount;
+        return true;
+    }
+} 
